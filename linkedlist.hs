f20a1e70f9ec1fd9ea8b329454939a6ab9513d7d
MariaSabbagh1 <maria.sabbagh2@net.usj.edu.lb>
2024-12-10 20:38:41 +0200

    Update linkedlist.hs

--- linkedlist.hs
+++ linkedlist.hs
@@ -1,88 +1,94 @@
-data Node a = Empty | Node a (Node a) deriving (Show)
+{-# LANGUAGE DeriveGeneric #-}
 
+import GHC.Generics (Generic)
+import Control.DeepSeq (NFData, rnf)
+import Criterion.Main
+import System.Random (randomRIO)
+
+-- Define the LinkedList data structure
+data Node a = Empty | Node a (Node a) deriving (Show, Generic)
 type Linkedlist a = Node a
 
-display :: Show a => Linkedlist a -> String
-display Empty = ""
-display (Node x Empty) = show x
-display (Node x xs) = show x ++ " , " ++ display xs
+-- Derive NFData for benchmarking
+instance NFData a => NFData (Node a) where
+    rnf Empty = ()
+    rnf (Node x next) = rnf x `seq` rnf next
 
+-- Append function
 append :: a -> Linkedlist a -> Linkedlist a
-append x Empty = Node x Empty --if the list is empty
-append x (Node y ys) = Node y (append x ys) --append the next node
+append x Empty = Node x Empty
+append x (Node y ys) = Node y (append x ys)
 
+-- Insert at the beginning
 insertFirst :: a -> Linkedlist a -> Linkedlist a
 insertFirst x Empty = Node x Empty
 insertFirst x (Node y ys) = Node x (Node y ys)
 
-
-insertLast :: a-> Linkedlist a -> Linkedlist a  
+-- Insert at the end
+insertLast :: a -> Linkedlist a -> Linkedlist a
 insertLast x Empty = Node x Empty
-insertLast x (Node y ys) = Node y (append x ys) 
+insertLast x (Node y ys) = Node y (append x ys)
 
+-- Delete the first element
 deleteFirst :: Linkedlist a -> Linkedlist a
 deleteFirst Empty = Empty
 deleteFirst (Node _ xs) = xs
 
+-- Delete the last element
 deleteLast :: Linkedlist a -> Linkedlist a
-deleteLastt Empty = Empty 
-deleteLast (Node _ Empty) = Empty --only one elt to remove
-deleteLast (Node x (Node _ Empty)) = Node x Empty --only two elts to remove
+deleteLast Empty = Empty
+deleteLast (Node _ Empty) = Empty
+deleteLast (Node x (Node _ Empty)) = Node x Empty
 deleteLast (Node x xs) = Node x (deleteLast xs)
 
-findByIndex :: Int -> Linkedlist a -> Maybe a
-findByIndex _ Empty = Nothing
-findByIndex 0 (Node x _) = Just x
-findByIndex n (Node _ xs)
-    | n > 0     = findByIndex (n - 1) xs  -- Recur for the next node
-    | otherwise = Nothing
-
-
+-- Reverse the list
 reverse1 :: Linkedlist a -> Linkedlist a
 reverse1 = rev1 Empty
-  where 
-     rev1 acc Empty = acc
-     rev1 acc (Node x xs) = rev1 (append x acc) xs
+  where
+    rev1 acc Empty = acc
+    rev1 acc (Node x xs) = rev1 (append x acc) xs
 
+-- Merge two linked lists
 merge :: Linkedlist a -> Linkedlist a -> Linkedlist a
 merge Empty ys = ys
 merge (Node x xs) ys = Node x (merge xs ys)
 
+-- Create a LinkedList from a list
+fromList :: [a] -> Linkedlist a
+fromList [] = Empty
+fromList (x:xs) = Node x (fromList xs)
 
+-- Generate a list of random integers
+generateRandomList :: Int -> Int -> Int -> IO [Int]
+generateRandomList n low high = sequence (replicate n (randomRIO (low, high)))
 
+-- Main function with benchmarks
 main :: IO ()
 main = do
-    let linkedList = Empty 
-    let linkedList1 = append 1 linkedList 
-    let linkedList2 = append 2 linkedList1
-    let linkedList3 = append 3 linkedList2  
-    let linkedList4 = insertFirst 0 linkedList3
-    let linkedList5 = insertLast 4 linkedList4
-    let linkedList6 = deleteFirst linkedList5
-    let linkedList7 = deleteLast linkedList6
-    let index =2
-    let invalidIndex = 5
-    let linkedList8 = reverse1 linkedList7
-    let linkedList9 = merge linkedList8 linkedList7
-    
-    putStrLn $ "list : [" ++ display linkedList3 ++ "]"
-    putStrLn $ "insert first : [" ++ display linkedList4 ++ "]"
-    putStrLn $ "insert last :[" ++ display linkedList5 ++ "]"
-    putStrLn $ "delete first : [" ++ display linkedList6 ++ "]"
-    putStrLn $ "delete last : [" ++ display linkedList7 ++ "]"
-    
+    randomList <- generateRandomList 1000 1 1000
+    let linkedList = fromList randomList
+    let linkedList2 = fromList randomList
 
-
-
-    case findByIndex index linkedList7 of
-        Just value -> putStrLn $ "Element at index " ++ show index ++ ": " ++ show value
-        Nothing -> putStrLn $ "Index " ++ show index ++ " is out of range"
-    
-   
-    case findByIndex invalidIndex linkedList7 of
-        Just value -> putStrLn $ "Element at index " ++ show invalidIndex ++ ": " ++ show value
-        Nothing -> putStrLn $ "Index " ++ show invalidIndex ++ " is out of range"
-
-  
-    putStrLn $ "reversed list [" ++ display linkedList8 ++ "]"
-    putStrLn $ "merged list [" ++ display linkedList9 ++ "]"
+    defaultMain
+        [ bgroup "append"
+            [ bench "append 1000 elements" $ nf (foldl (\acc x -> append x acc) linkedList) randomList
+            ]
+        , bgroup "insertFirst"
+            [ bench "insertFirst 1000 elements" $ nf (foldl (\acc x -> insertFirst x acc) linkedList) randomList
+            ]
+        , bgroup "insertLast"
+            [ bench "insertLast 1000 elements" $ nf (foldl (\acc x -> insertLast x acc) linkedList) randomList
+            ]
+        , bgroup "deleteFirst"
+            [ bench "deleteFirst 1000 elements" $ nf (foldl (\acc _ -> deleteFirst acc) linkedList) [1..1000]
+            ]
+        , bgroup "deleteLast"
+            [ bench "deleteLast 1000 elements" $ nf (foldl (\acc _ -> deleteLast acc) linkedList) [1..1000]
+            ]
+        , bgroup "reverse1"
+            [ bench "reverse1" $ nf reverse1 linkedList
+            ]
+        , bgroup "merge"
+            [ bench "merge two linked lists" $ nf (merge linkedList) linkedList2
+            ]
+        ]
